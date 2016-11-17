20ee3c550c1981d7840cc1b3cee76ea2b1442dd2
Shivaal Roy <shivaal@stanford.edu>
2016-11-17 12:05:27 -0800

    fixed n-number businesses

--- parse.py
+++ parse.py
@@ -8,91 +8,101 @@ from json import dumps
 from re import sub
 import random
 
-def parsebusiness(json_file):
-    businesses = []
+def parsebusiness(json_file, numBusinesses=100):
+    data = []
+    localeIds = set()
     with open(json_file, 'r') as f:
         for line in f:
-            businesses.append(loads(line))
-    print 'Total businesses before', len(businesses)
-
-    data = []
-    businessids = set()
-    for business in businesses:
-        if ('Restaurants' in business['categories']) and (business['city'] == 'Las Vegas'):
-            data.append(business)
-            businessids.add(business['business_id'])
-
+            business = loads(line)
+            if ('Restaurants' in business['categories']) and (business['city'] == 'Las Vegas'):
+                data.append(business)
+                localeIds.add(business['business_id'])
     print 'Total Restaurants: ', len(data)
-    random.shuffle(data)
-    data = data[:100]
-    print 'Shrunk it to:', len(data)
-    data_file = open('yelp_data/restaurant_data.json','w')
-    for business in data:
-        data_file.write(dumps(business, data_file))
-        data_file.write('\n')
-    return businessids
+
+    sampled_data = random.sample(data, numBusinesses)
+    print 'Shrunk it to:', len(sampled_data)
+
+    businessids = set()
+    for business in sampled_data:
+        businessids.add(business['business_id'])
+
+    with open('curated-data/restaurant_data.json','w') as data_file:
+        for business in sampled_data:
+            data_file.write(dumps(business, data_file))
+            data_file.write('\n')
+    return businessids, localeIds
+
+def filterLocaleReviews(json_file, localeIds):
+    lineCount = 0
+    with open(json_file, 'r') as readFile, open('yelp-data/las_vegas_reviews.json', 'w') as writeFile:
+        for line in readFile:
+            review = loads(line)
+            if review['business_id'] in localeIds:
+                writeFile.write(dumps(review))
+                writeFile.write('\n')
+                lineCount += 1
+    print 'Las Vegas reviews:', lineCount
+
 
 def parsereview(json_file, businessids):
-    reviews = []
+    data = []
     edges = []
+    userids = set()
     with open(json_file, 'r') as f:
         for line in f:
-            reviews.append(loads(line))
-    print 'Total reviews before', len(reviews)
+            review = loads(line)
+            if review['business_id'] in businessids:
+                data.append(review)
+                edges.append(str(review['user_id'])+'\t'+str(review['business_id']))
+                userids.add(review['user_id'])
+    print 'Total Reviews:', len(data)
 
-    data = []
-    userids = set()
-    for review in reviews:
-        if review['business_id'] in businessids:
-            data.append(review)
-            userids.add(review['user_id'])
-            edges.append(str(review['user_id'])+' '+str(review['business_id'])+'\n')
-
-    print 'Total Reviews: ', len(data)
-    data_file = open('yelp_data/review_data.json','w')
-    for review in data:
-        data_file.write(dumps(review, data_file))
-        data_file.write('\n')
-    edges_file = open('yelp_data/reviews.txt','w')
-    for edge in edges:
-        edges_file.write(edge)
+    with open('curated-data/review_data.json','w') as data_file:
+        for review in data:
+            data_file.write(dumps(review, data_file))
+            data_file.write('\n')
+    with open('curated-data/reviews.txt','w') as edges_file:
+        for edge in edges:
+            edges_file.write(edge)
+            edges_file.write('\n')
     return userids
 
 def parseuser(json_file, userids):
-    users = []
+    data = []
     edges = []
     with open(json_file, 'r') as f:
         for line in f:
-            users.append(loads(line))
+            user = loads(line)
+            if user['user_id'] in userids:
+                data.append(user)
+                for friend in user['friends']:
+                    if user['user_id'] < friend:
+                        edges.append(user['user_id']+'\t'+friend)
+    print 'Total Users:', len(data)
 
-    print 'Total users before', len(users)
-    data = []
-    for user in users:
-        if user['user_id'] in userids:
-            data.append(user)
-            for friend in user['friends']:
-                if user['user_id'] < friend:
-                    edges.append(user['user_id']+' '+friend+'\n')
-
-    print 'Total Users: ', len(data)
-    data_file = open('yelp_data/user_data.json','w')
-    for user in data:
-        data_file.write(dumps(user, data_file))
-        data_file.write('\n')
-    edges_file = open('yelp_data/friends.txt','w')
-    for edge in edges:
-        edges_file.write(edge)
+    with open('curated-data/user_data.json','w') as data_file:
+        for user in data:
+            data_file.write(dumps(user, data_file))
+            data_file.write('\n')
+    with open('curated-data/friends.txt','w') as edges_file:
+        for edge in edges:
+            edges_file.write(edge)
+            edges_file.write('\n')
 
 def main(argv):
     f = 'yelp-data/yelp_academic_dataset_business.json'
-    businessids = parsebusiness(f)
-    print 'Success parsing ' + f
-    f2 = 'yelp-data/yelp_academic_dataset_review.json'
+    print 'Parsing ' + f
+    businessids, localeIds = parsebusiness(f, 100)
+    print 'Done parsing ' + f
+    f2 = 'yelp-data/las_vegas_reviews.json'
+    print 'Parsing ' + f2
     userids = parsereview(f2, businessids)
-    print 'Success parsing', f2
+    print 'Done parsing', f2
     f3 = 'yelp-data/yelp_academic_dataset_user.json'
+    print 'Parsing ' + f3
     parseuser(f3, userids)
-    print 'Success parsing', f3
+    print 'Done parsing', f3
+    # filterLocaleReviews(f2, localeIds)
 
 if __name__ == '__main__':
     main(sys.argv)
